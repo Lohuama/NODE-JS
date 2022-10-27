a2d8ee262a63045d08c10eefeaaf3eaa6d531718
Lohuama <limalorruama98@gmail.com>
2022-10-27 12:55:45 -0300

    estudos

---
+++ mais_sobre_modulos/calc.js
@@ -0,0 +1,4 @@
+
+module.exports .soma = function soma(x,y){return x + y}
+module.exports .mult = function mult(x,y){return x * y}
+
---
+++ modulos/calc.js
@@ -0,0 +1,15 @@
+
+let calc = {
+    mult: (x,y) => {
+        return x * y
+    },
+
+    soma: (x,y) => {
+        return x + y
+    }
+}
+
+
+
+
+module.exports = calc
