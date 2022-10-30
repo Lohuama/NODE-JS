02d4f13b3f80bd26ae09a643e4a7f8869bf0a44f
Lohuama <limalorruama98@gmail.com>
2022-10-30 11:32:49 -0300

    introducao a nodejs finalizado

---
+++ get-post-put-delete/index.js
@@ -0,0 +1,52 @@
+const http = require('http')
+const url = require('url')
+const fs = require('fs')
+
+function handleFile(req, res, callback){
+    let path = url.parse(req.url).pathname
+    let fileName = '.'+path;
+
+    fs.readFile(fileName, (err, data)=>{
+        if(err){
+
+            if(callback){
+               if(!callback(req, res)){
+                res.writeHead(404, {'Content-Type':"text/html;charset:utf-8"})
+                res.end("<h1>Vish, deu ruim</h1>")
+               }
+            }
+            
+        }else{
+            res.writeHead(200, {'Content-Type':"text/html"})
+            res.write(data)
+            res.end("<h1>Ihu, arrasô</h1>")
+        }
+    })
+}
+
+function handleRequest(req, res){
+    let path = url.parse(req.url).pathname
+
+    let method = req.method;
+    console.log(method)
+    if(method == 'PUT'){
+        res.writeHead(404, {'Content-Type':"text/html;charset:utf-8"})
+    }
+
+    if(path == '/teste'){
+        res.end("Teste");
+        return true;
+    }
+    return false;
+}
+
+http.createServer((requeste, response)=>{
+    handleFile(requeste, response, handleRequest)
+
+}).listen(3000, (err)=>{
+    if(err){
+        console.log(err)
+    }else{
+        console.log('Roda Regina, roda')
+    }
+})
